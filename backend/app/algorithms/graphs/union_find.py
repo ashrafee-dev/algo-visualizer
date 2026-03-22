c249018776939957049e17e58665ab1561b62be3
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-03-22 08:33:52 -0400

    chore: scaffold DFS, Kruskal, Prim, topological sort, and union-find modules

---
+++ backend/app/algorithms/graphs/union_find.py
@@ -0,0 +1,21 @@
+METADATA = {
+    "id": "graphs.union_find",
+    "name": "Union Find",
+    "category": "Graphs",
+    "description": "Coming soon algorithm scaffold.",
+    "status": "todo",
+    "time_complexity": "TBD",
+    "space_complexity": "TBD",
+    "tags": ["todo"],
+    "visualization_type": "custom",
+    "default_input": {},
+    "example_inputs": [],
+}
+
+SOURCE_CODE = """def run_algorithm(input_data, emitter):
+    raise NotImplementedError('Coming soon')
+"""
+
+
+def run(input_data):
+    raise NotImplementedError("Coming soon")
